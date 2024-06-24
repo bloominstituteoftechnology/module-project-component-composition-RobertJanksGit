3a0ab663240eb6fd0afc50f25f070a598ffcbb03
Robert Jank <robert.jank@yahoo.com>
2024-06-24 11:14:04 -0500

    First commit

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from "react";
 
 function App() {
   return (
     <p>
       Read through the instructions in the README.md file to build your NASA
-      app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+      app! Have fun{" "}
+      <span role="img" aria-label="go!">
+        🚀
+      </span>
+      !
     </p>
-  )
+  );
 }
 
-export default App
+export default App;
